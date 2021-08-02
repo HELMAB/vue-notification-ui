5e8d5971f22e739441085a0aff6f54236dd89757
HEL Mab <mabhelitc@gmail.com>
2021-08-02 20:28:03 +0700

    sfc-init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueNotificationUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueNotificationUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-notification-ui-sample />
+  </div>
+</template>
